1b4bdbf43b5aee27977affff68bc70e70eabad79
alcides_t <alcides.dml@icloud.com>
2022-07-27 08:28:08 +0900

    Add changes toto webpage using API

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function() {
-    let usersRequest = await fetch (
-        "https://randomuser.me/api?results=5");
-        let data = await usersRequest.json();
+
+
+const getData = async function(numUsers) {
+    const usersRequest = await fetch (
+        `https://randomuser.me/api?results=${numUsers}`
+        );
+        const data = await usersRequest.json();
     
-    const userResults = data.results;
-    console.log(userResults);
+    const userResults = data.results; // array of objects
+    // console.log(userResults);
     displayUsers(userResults);
 };
-getData();
+
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
     for (const user of userResults) {
-        const country = user.location.country;
+        const country = user.location.country; 
         const name = user.name.first;
         const imageUrl = user.picture.medium;
         const userDiv = document.createElement("div");
         userDiv.innerHTML = `
         <h3>${name}</h3>
         <p>${country}</p>
-        <img src=${imageUrl} alt="User avatar" />`;
+        <img src=${imageUrl} alt="User avatar" />
+        `;
         randomFolks.append(userDiv);
     }
-}
+};
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
+
 
 
